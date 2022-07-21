07f048615249727fc0ff718ebe644eeaffa6baf9
Matheus Rocha <01devmath@gmail.com>
2022-07-20 23:57:36 -0300

    add login function; add email to user and validate input

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    trim: true,
     required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid.');
+      }
+    }
   },
   password: {
     minLength: 6,
@@ -14,16 +27,37 @@ const userSchema = new mongoose.Schema({
     type: String,
     validate(value) {
       if (value.includes("password")) {
-        throw new Error("Password is invalid");
+        throw new Error("Password is invalid.");
       }
     },
   },
   age: {
     type: Number,
     default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    }
   },
 });
 
+// login
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('unable to login');
+  }
+}
+
+// hash password before saving
 userSchema.pre('save', async function (next) {
   const user = this;
 
