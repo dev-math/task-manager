53b2a8388f3b2906109647cfbb261e85a0ba0967
Matheus Rocha <01devmath@gmail.com>
2022-07-21 00:10:16 -0300

    set login route

--- src/models/user.js
+++ src/models/user.js
@@ -47,14 +47,16 @@ userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
 
   if (!user) {
-    throw new Error('unable to login');
+    throw new Error('Unable to login');
   }
 
   const isMatch = await bcrypt.compare(password, user.password)
 
   if (!isMatch) {
-    throw new Error('unable to login');
+    throw new Error('Unable to login');
   }
+  
+  return user;
 }
 
 // hash password before saving
--- src/routers/user.js
+++ src/routers/user.js
@@ -14,6 +14,16 @@ router.post('/users', async (req, res) => {
   }
 });
 
+// login user
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 // read users
 router.get('/users', async (_req, res) => {
   try {
